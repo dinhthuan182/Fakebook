5494767b0d9c66f950b2e41eb3eb32a7945cb804
Dinh Thuan <dinhthuan2020@gmail.com>
2019-07-07 23:36:35 +0700

    new project, using devise for users, upload avatar with paperclip and upload photo, album with active storage.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+   def index
+    @users = User.all
+  end
+
+  def show
+    if params[:id].nil? && current_user
+      @user = current_user
+    else
+      @user = User.find(params[:id])
+    end
+  end
+end
