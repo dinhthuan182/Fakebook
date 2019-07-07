7c5b8e632983c44c80ddb0944322fea0bf95ecba
Dinh Thuan <dinhthuan2020@gmail.com>
2019-07-07 23:31:15 +0700

    delete project

--- README.md
+++
@@ -1,7 +0,0 @@
-# README
-
-Final App (Focobook)
-Họ tên: Vũ Đình Thuận
-email: dinhthuan2020@gmail.com
-SDT: 0846473479
-
--- app/models/like.rb
+++
@@ -1,6 +0,0 @@
-class Like < ApplicationRecord
-#Association
-  belongs_to :user
-  belongs_to :postable, polymorphic: true
-#Validates
-end
