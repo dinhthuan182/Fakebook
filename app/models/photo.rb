daeb0726d7931c821862b445357ed56bbb984755
Dinh Thuan <dinhthuan2020@gmail.com>
2019-07-19 13:56:27 +0700

    completed button like

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,9 @@
 class Photo < ApplicationRecord
   belongs_to :user
-  has_many :likes, as: :likeable
+  has_many :likes, as: :likeable, dependent: :destroy
   has_one_attached :image
+
+  # def likes_by? user
+  #   likes.exists? user: user
+  # end
 end
