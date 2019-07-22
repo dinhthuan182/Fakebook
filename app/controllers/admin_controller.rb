4146c604187fcaf610aadb6b687371d067a11380
Dinh Thuan <dinhthuan2020@gmail.com>
2019-07-22 18:56:43 +0700

    code error

--- app/controllers/admin_controller.rb
+++
@@ -1,7 +0,0 @@
-class AdminController < ApplicationController
-  def index
-    @photos = Photo.all.paginate(page: params[:page], per_page: 10)
-    @albums = Album.all.paginate(page: params[:page], per_page: 10)
-    @users = User.all.paginate(page: params[:page], per_page: 10)
-  end
-end
--- db/migrate/20190719070442_add_default_for_user.rb
+++
@@ -1,9 +0,0 @@
-class AddDefaultForUser < ActiveRecord::Migration[5.2]
-  def up
-    change_column :users, :activated, :boolean, default: true
-  end
-
-  def down
-    change_column :users, :activated, :boolean, default: nil
-  end
-end
